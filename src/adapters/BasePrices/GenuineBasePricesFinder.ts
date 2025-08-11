b113e087389637a53a218402d43f11912f4bed86
Damien Pobel <damien@pobel.fr>
2025-08-11 18:36:28 +0200

    feat: price change in August and September 2025

--- src/adapters/BasePrices/GenuineBasePricesFinder.ts
+++ src/adapters/BasePrices/GenuineBasePricesFinder.ts
@@ -16,10 +16,24 @@ const between20250201and20250314 = {
   solar: 0.1269,
 };
 
-const after20250314 = {
+const between20250314and20250801 = {
   label: "Après le 14 mars 2025",
   offPeakHours: 0.1727,
   peakHours: 0.2124,
+  solar: 0.1269,
+};
+
+const between20250801and20250815 = {
+  label: "Après le 1er août 2025",
+  offPeakHours: 0.1682,
+  peakHours: 0.2079,
+  solar: 0.1269,
+};
+
+const after20250915 = {
+  label: "Après le 15 septembre 2025",
+  offPeakHours: 0.1637,
+  peakHours: 0.2082,
   solar: 0.1269,
 };
 
@@ -27,7 +41,7 @@ export class GenuineBasePricesFinder implements BasePricesFinder {
   findForMonth(month: Month) {
     const march2025 = new Month(3, 2025);
     if (month.isEqual(march2025)) {
-      return [between20250201and20250314, after20250314];
+      return [between20250201and20250314, between20250314and20250801];
     }
     const february2025 = new Month(2, 2025);
     if (month.isEqual(february2025)) {
@@ -36,7 +50,18 @@ export class GenuineBasePricesFinder implements BasePricesFinder {
     if (month.isBefore(february2025)) {
       return [before20250201];
     }
-    return [after20250314];
+    const august2025 = new Month(8, 2025);
+    if (month.isBefore(august2025)) {
+      return [between20250314and20250801];
+    }
+    if (month.isEqual(august2025)) {
+      return [between20250801and20250815];
+    }
+    const september2025 = new Month(9, 2025);
+    if (month.isEqual(september2025)) {
+      return [between20250801and20250815, after20250915];
+    }
+    return [after20250915];
   }
 
   findForDay(day: Day) {
@@ -46,6 +71,12 @@ export class GenuineBasePricesFinder implements BasePricesFinder {
     if (day.isBefore(new Day(new Month(3, 2025), 14))) {
       return between20250201and20250314;
     }
-    return after20250314;
+    if (day.isBefore(new Day(new Month(8, 2025), 1))) {
+      return between20250314and20250801;
+    }
+    if (day.isBefore(new Day(new Month(9, 2025), 15))) {
+      return between20250801and20250815;
+    }
+    return after20250915;
   }
 }
