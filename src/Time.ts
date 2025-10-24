19664c1167096f519950f30545304f7050548ccc
Damien Pobel <damien@pobel.fr>
2025-10-24 23:24:42 +0200

    fix: electricity consumption fetching fails on DST end day

--- src/Time.ts
+++ src/Time.ts
@@ -33,6 +33,16 @@ export class Time {
     );
   }
 
+  addMinutes(minutesToAdd: number): Time {
+    const totalMinutes = this.hour * 60 + this.minute + minutesToAdd;
+    const newHour = Math.floor(totalMinutes / 60) % 24;
+    const newMinute = totalMinutes % 60;
+    if (newHour === 0 && newMinute === 0 && this.second === 0) {
+      return new Time(23, 59, 59);
+    }
+    return new Time(newHour, newMinute, this.second);
+  }
+
   toString(): string {
     return `${this.hour.toString().padStart(2, "0")}:${this.minute
       .toString()
